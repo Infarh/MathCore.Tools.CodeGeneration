e7723894c3630ff15944a1676f8b97af9813a50c
Infarh <shmachilin@gmail.com>
2024-06-29 17:06:11 +0300

    Методы для работы со строками

---
+++ MathCore.Tools.CodeGeneration/Extensions/Infrastructure/StringBuilderEx.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace MathCore.Tools.CodeGeneration.Extensions.Infrastructure;
+
+public static class StringBuilderEx
+{
+    public static StringBuilder Append(this StringBuilder builder, string Format, object arg0) => builder.AppendFormat(Format, arg0);
+
+    public static StringBuilder Append(this StringBuilder builder, string Format, object arg0, object arg1) => builder.AppendFormat(Format, arg0, arg1);
+
+    public static StringBuilder Append(this StringBuilder builder, string Format, object arg0, object arg1, object arg2) => builder.AppendFormat(Format, arg0, arg1, arg2);
+
+    public static StringBuilder Append(this StringBuilder builder, string Format, params object[] args) => builder.AppendFormat(Format, args);
+
+    public static StringBuilder AppendLine(this StringBuilder builder, string Format, object arg0) => builder.AppendFormat(Format, arg0);
+
+    public static StringBuilder AppendLine(this StringBuilder builder, string Format, object arg0, object arg1) => builder.AppendFormat(Format, arg0, arg1);
+
+    public static StringBuilder AppendLine(this StringBuilder builder, string Format, object arg0, object arg1, object arg2) => builder.AppendFormat(Format, arg0, arg1, arg2);
+
+    public static StringBuilder AppendLine(this StringBuilder builder, string Format, params object[] args) => builder.AppendFormat(Format, args);
+
+    public static StringBuilder LN(this StringBuilder builder) => builder.AppendLine();
+    
+    public static StringBuilder LN(this StringBuilder builder, string str) => builder.AppendLine(str);
+    
+    public static StringBuilder LN(this StringBuilder builder, string Format, object arg0) => builder.Append(Format, arg0).LN();
+    
+    public static StringBuilder LN(this StringBuilder builder, string Format, object arg0, object arg1) => builder.Append(Format, arg0, arg1).LN();
+    
+    public static StringBuilder LN(this StringBuilder builder, string Format, object arg0, object arg1, object arg2) => builder.Append(Format, arg0, arg1, arg2).LN();
+    
+    public static StringBuilder LN(this StringBuilder builder, string Format, params object[] args) => builder.Append(Format, args).LN();
+
+    public static string ToStringWithLineNumbers(this StringBuilder builder, char Separator = '|') =>
+        builder
+            .ToString()
+            .Split([StringEx.NewLine], StringSplitOptions.None)
+            .Select((s, i) => $"{i + 1,3}{Separator}{s}")
+            .JoinStringLN();
+}
---
+++ MathCore.Tools.CodeGeneration/Extensions/StringEx.cs
@@ -0,0 +1,29 @@
+﻿namespace MathCore.Tools.CodeGeneration.Extensions;
+
+public static class StringEx
+{
+    public static string NewLine { get; set; } = "\r\n";
+
+    public static string TrimEnd(this string str, string trim, StringComparison Comparison = StringComparison.Ordinal)
+    {
+        if (!str.EndsWith(trim, Comparison) || string.IsNullOrEmpty(trim)) return str;
+        if (str == trim) return string.Empty;
+
+        return str.Substring(0, str.Length - trim.Length);
+    }
+
+    public static IEnumerable<string> EnumLines(this string str)
+    {
+        var reader = new StringReader(str);
+        while (reader.ReadLine() is { } line)
+            yield return line;
+    }
+
+    public static IEnumerable<T> EnumLines<T>(this string str, Func<string, T> selector) => str.EnumLines().Select(selector);
+
+    public static IEnumerable<T> EnumLines<T>(this string str, Func<string, int, T> selector) => str.EnumLines().Select(selector);
+
+    public static string JoinString(this IEnumerable<string> strings, string separator) => string.Join(separator, strings);
+
+    public static string JoinStringLN(this IEnumerable<string> strings) => string.Join(NewLine, strings);
+}
